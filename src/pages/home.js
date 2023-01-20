e4d692add4cacbd943fc226017af559a648a926c
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-01-20 12:08:34 +0100

    fix export default bug for home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { LayoutHome } from "../components/struct/layout.js";
 
-export function Home() {
+export default function Home() {
   return <LayoutHome>
     <h1>Back Home</h1>
   </LayoutHome>;
