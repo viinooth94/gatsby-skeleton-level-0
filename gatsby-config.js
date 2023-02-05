3f248f6762231d488f7920bcf0018db9fc1b3e19
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-02-05 17:20:53 +0100

    remove dead code

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Skeleton`,
     siteUrl: `https://squelette.netlify.app/`,
-    version: `0.0.1`,
+    version: `0.0.2`,
     author: `stanislas Marçais`,
   },
   plugins: [
