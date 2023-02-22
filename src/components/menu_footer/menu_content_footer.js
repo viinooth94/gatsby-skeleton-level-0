79de32769073c5a6245570f6d7af370b9c0f9f98
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-02-22 12:57:51 +0100

    all the language option work, but not... because need to put the Provider at the top

--- src/components/menu_footer/menu_content_footer.js
+++ src/components/menu_footer/menu_content_footer.js
@@ -1,28 +1,18 @@
+// REACT
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { useContext } from "react";
+// APP
 import { NavCell } from "../gui";
+import tree from "./../../../media/tree.json";
+import { RegionContext } from "../../context";
 
 export function Legal() {
+	const { lang } = useContext(RegionContext);
+
 	const style_cell = {
 		padding : "0 1em",
 		cursor: "pointer",
 	}
 	
-	const data = useStaticQuery(
-    graphql`
-		query {
-			allMarkdownRemark(filter: {frontmatter: {categorie: {eq: "legal"}}}) {
-				edges {
-					node {
-						frontmatter {
-							title
-						}
-					}
-				}
-			}
-		}
-    `
-	)
-	console.log("data legal", data);
-	return <NavCell to="/legal" style={style_cell}>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</NavCell>
+	return <NavCell to="/legal" style={style_cell}>{tree[lang].legal}</NavCell>
 }
--- src/components/menu_header/menu_region.js
+++ src/components/menu_header/menu_region.js
@@ -1,21 +1,32 @@
 // REACT
 import React from "react";
+import { useContext } from "react";
 // APP
-import { Box } from "./../gui"
+import { Box } from "./../gui";
+import { RegionContext } from "../../context";
 
-function Cell ({className_box, style_box, className_cell, style_cell, children}) {
+// we cannot use key for the props because it's react reserved word
+function Cell ({className_box, style_box, className_cell, style_cell, keys, index, children}) {
+	const { set_lang } = useContext(RegionContext);
+	function mouse_click(event) {
+		event.preventDefault();
+		set_lang(keys[index]);
+	}
+
 	return <Box className={className_box} style={style_box}>
-		<div className={className_cell} style={style_cell}>
+		<div className={className_cell} style={style_cell} onClick={mouse_click}>
 			{children}
 		</div>
 	</Box>
 }
 
-export function MenuRegion({className_box, style_box, className_cell, style_cell, content}) {
+export function MenuRegion({className_box, style_box, className_cell, style_cell, values, keys}) {
+	// we cannot use key for the props because it's react reserved word
 	return <>
-		{content.map((elem, key) => {
+		{values.map((elem, key) => {
 			return <Cell className_box={className_box} style_box={style_box} 
-				className_cell={className_cell} style_cell={style_cell}>
+										className_cell={className_cell} style_cell={style_cell}
+										keys={keys} index={key}>
 					{elem}
 				</Cell>
 		})}
